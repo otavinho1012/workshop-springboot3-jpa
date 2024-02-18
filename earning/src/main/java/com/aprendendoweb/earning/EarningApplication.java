d462b37d5002488a60c11e5a196b1d4ca096b402
Otavio Augusto <oaugusto265@gmail.com>
2024-02-18 18:46:57 -0300

    First commit

---
+++ earning/src/main/java/com/aprendendoweb/earning/EarningApplication.java
@@ -0,0 +1,13 @@
+package com.aprendendoweb.earning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarningApplication.class, args);
+	}
+
+}
---
+++ earning/src/test/java/com/aprendendoweb/earning/EarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendoweb.earning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
